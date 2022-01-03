b1735a475931e67c247a0db0eeffffb4babcebf0
Daphne Orme <dnow2000@gmail.com>
2022-01-02 20:36:14 -0500

    First commit

---
+++ app/src/main/java/com/example/orme_degree_tracker/HomeViewModel.java
@@ -0,0 +1,12 @@
+package com.example.orme_degree_tracker;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class HomeViewModel extends AndroidViewModel {
+    public HomeViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
